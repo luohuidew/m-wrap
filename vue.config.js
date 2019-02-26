afacaae4748f55b2fe822acabbddd156f09e2463
heyafei <abel.he@weiget.com>
2019-02-26 16:10:25 +0800

    qudiao config

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,18 @@ module.exports = {
   // 选项...
   baseUrl: process.env.NODE_ENV === 'production' ?
     '' : '/',  
-
   configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      /* github上提示的删除提示的方法，不可用 */
-      config.optimization.minimizer[0].options.extractComments = true;
-      config.optimization.minimizer[0].options.uglifyOptions.output.comments = false;
-      config.optimization.minimizer[0].options.uglifyOptions.compress = {
-        warnings: false,
-        drop_debugger: false,
-        drop_console: false,
-        pure_funcs: ['console.log']
-      };
-      config.optimization.minimizer[0].options.sourceMap = false;
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   /* github上提示的删除提示的方法，不可用 */
+    //   config.optimization.minimizer[0].options.extractComments = true;
+    //   config.optimization.minimizer[0].options.uglifyOptions.output.comments = false;
+    //   config.optimization.minimizer[0].options.uglifyOptions.compress = {
+    //     warnings: false,
+    //     drop_debugger: false,
+    //     drop_console: false,
+    //     pure_funcs: ['console.log']
+    //   };
+    //   config.optimization.minimizer[0].options.sourceMap = false;
+    // }
   }
 }
